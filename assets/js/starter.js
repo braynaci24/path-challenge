5e40348ff352e79ba6121bdb47a2d2a31daa9f16
braynaci24 <aynacimuhammetemin@gmail.com>
2021-05-06 17:04:53 +0300

    directory fix

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -8,7 +8,7 @@
 //
 // Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
 
-import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./bootstrap.bundle.min.js";
 
 
 // Option 2
@@ -18,6 +18,6 @@ import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 // If you're importing tooltips or popovers, be sure to include our Popper.js dependency
 // import "../../node_modules/popper.js/dist/popper.min.js";
 
-import "../../node_modules/bootstrap/js/dist/util.js";
-import "../../node_modules/bootstrap/js/dist/modal.js";
+import "./util.js";
+import "./modal.js";
 
